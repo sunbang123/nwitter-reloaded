fbb11200e66ec8e0862d97bf363456ecf930f45a
sunbang123 <musicianksy@naver.com>
2024-07-08 18:49:55 +0900

    add firebase

---
+++ src/routes/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvaBuQHq-WZRaB8tO1TSkvPr5oz4T5K5Q",
+  authDomain: "stickers-9f4a7.firebaseapp.com",
+  projectId: "stickers-9f4a7",
+  storageBucket: "stickers-9f4a7.appspot.com",
+  messagingSenderId: "350239623114",
+  appId: "1:350239623114:web:3afccd9ab562d02dd67133"
+};
+
+const app = initializeApp(firebaseConfig);

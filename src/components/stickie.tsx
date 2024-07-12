681fe1ff53908ccd608867faf18d3cdd52fc221b
sunbang123 <musicianksy@naver.com>
2024-07-12 13:23:00 +0900

    수정 ui

--- src/components/stickie.tsx
+++ src/components/stickie.tsx
@@ -3,7 +3,7 @@ import { IStickie } from "./timeline";
 import { auth, db, storage } from "../firebase";
 import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
-
+import { useState } from "react";
 
 const Wrapper = styled.div`
     display: grid;
@@ -11,6 +11,31 @@ const Wrapper = styled.div`
     padding: 20px;
     border: 1px solid rgba(255, 255, 255, 0.5);
     border-radius: 15px;
+`;
+
+const Form = styled.form<{ show: String }>`
+    display: ${props => props.show ? 'flex' : 'none'};
+    flex-direction: column;
+    gap: 10px;
+`;
+
+const TextArea = styled.textarea`
+    border: 2px solid ;
+    padding: 20px;
+    margin-top:20px;
+    font-size: 16px;
+    color: black;
+    background-color: white;
+    width: 100%;
+    resize: none;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    &::placeholder {
+        font-size: 16px;
+    }
+    &:focus{
+        outline: none;
+        border-color: #3C950B;
+    }
 `;
 
 const Column = styled.div`
@@ -30,12 +55,14 @@ const Username = styled.span`
     font-size: 15px;
 `;
 
-const Payload = styled.p`
+const Payload = styled.p<{ show: String }>`
+    display: ${props => props.show ? 'flex' : 'none'};
     margin: 10px 0px;
     font-size: 18px;
 `;
 
-const DeleteButton = styled.button`
+const DeleteButton = styled.button<{ show: String }>`
+    display: ${props => props.show ? 'flex' : 'none'};
     background-color: tomato;
     color:white;
     font-weight: 600;
@@ -62,6 +89,8 @@ const EditButton = styled.button`
 
 export default function Stickie({ username, photo, stickie, userId, id }: IStickie) {
     const user = auth.currentUser;
+    const [isEditing, setIsEditing] = useState(false);
+    
     const onDelete = async () => {
         const ok = confirm("Are you sure you want to delete this stickie?");
         if(!ok || user?.uid !== userId) return;
@@ -78,29 +107,30 @@ export default function Stickie({ username, photo, stickie, userId, id }: IStick
         }
     };
 
-    const onEdit = async () => {
-        const ok = confirm("Are you sure you want to edit this stickie?");
-        if (!ok || user?.uid !== userId) return;
-        const newContent = prompt("Enter the new content for the stickie:");
-        if (!newContent) return;
-        try {
-            await updateDoc(doc(db, "stickies", id), { 
-                stickie: newContent
-             });
-        } catch (e) {
-            console.log(e);
-        } finally {
-
-        }
+    const onEdit = () => {
+        setIsEditing(prev => !prev);
     };
     
     return (
         <Wrapper>
             <Column>
                 <Username>{username}</Username>
-                <Payload>{stickie}</Payload>
+                <Payload show={isEditing ? "" : "show"}>{stickie}</Payload>
                 {user?.uid == userId? (
-                    <><DeleteButton onClick={onDelete}>Delete</DeleteButton><EditButton onClick={onEdit}>Edit</EditButton></>
+                    <>
+                        <DeleteButton show={isEditing ? "" : "show"} onClick={onDelete}>Delete</DeleteButton>
+                        <EditButton onClick={onEdit}>Edit</EditButton>
+                        <Form show={isEditing ? "show" : ""}>
+                            <TextArea 
+                                required
+                                rows={5}
+                                maxLength={180}
+                                // onChange={onChange}
+                                // value={stickie}
+                                placeholder="What is happening?!"
+                            />
+                        </Form>
+                    </>
                 ) : null}
             </Column>
             <Column>{photo ? <Photo src={photo}/> : null}</Column>
